145a79bec48b6f128334a47df06b332640e08eb4
Christian Parpart <trapni@gmail.com>
2016-04-14 15:31:50 +0200

    fixes a problem for exposed port count changes in a marathon app

--- haproxy_manager.go
+++ haproxy_manager.go
@@ -497,6 +497,13 @@ func (tasks SortedTaskList) Len() int {
 func (tasks SortedTaskList) Less(i, j int) bool {
 	var a = &tasks.Tasks[i]
 	var b = &tasks.Tasks[j]
+
+	if len(a.Ports) < tasks.PortIndex || len(b.Ports) < tasks.PortIndex {
+		// XXX That's a very case; when you redeploy your app with the port count
+		// changed, you might run into here.
+		return false
+	}
+
 	return a.Host < b.Host || a.Ports[tasks.PortIndex] < b.Ports[tasks.PortIndex]
 }
 
