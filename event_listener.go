c457449706c9441c7c8d2e4d0058d16f713f09ee
Christian Parpart <trapni@gmail.com>
2016-09-19 22:29:30 +0200

    documentation update

--- event_listener.go
+++ event_listener.go
@@ -11,13 +11,20 @@ type EventListener interface {
 	Shutdown()
 
 	// Apply installs the load balancer for all apps.
+	//
 	// This function is invoked upon startup to synchronize with the current
 	// state.
 	Apply(apps []*AppCluster)
 
 	// AddTask must add the given backend to the cluster.
+	//
+	// It is assured that the task to be added is also already added to the
+	// given AppCluster.
 	AddTask(task *AppBackend, app *AppCluster)
 
 	// RemoveTask must remove the given backend from the cluster.
+	//
+	// It is ensured that the task to be removed is not present in the given
+	// AppCluster.
 	RemoveTask(task *AppBackend, app *AppCluster)
 }
