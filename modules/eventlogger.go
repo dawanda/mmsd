c189804ad37be8e81dffcdd3ff990d9115e60d5b
Christian Parpart <trapni@gmail.com>
2016-09-20 20:11:55 +0200

    slightly improved eventlogger output (just prefixes, yeah)

--- modules/eventlogger.go
+++ modules/eventlogger.go
@@ -15,32 +15,32 @@ type EventLoggerModule struct {
 }
 
 func (logger *EventLoggerModule) Startup() {
-	log.Printf("Initialize\n")
+	log.Printf("eventlogger: Initialize\n")
 }
 
 func (logger *EventLoggerModule) Shutdown() {
-	log.Printf("Shutdown\n")
+	log.Printf("eventlogger: Shutdown\n")
 }
 
 func (logger *EventLoggerModule) Apply(apps []*core.AppCluster) {
 	if logger.Verbose {
 		out, err := json.MarshalIndent(apps, "", "  ")
 		if err != nil {
-			log.Printf("Marshal failed. %v\n", err)
+			log.Printf("eventlogger: Marshal failed. %v\n", err)
 		} else {
-			fmt.Printf("%v\n", string(out))
+			fmt.Printf("eventlogger: %v\n", string(out))
 		}
 	}
 
 	for _, app := range apps {
-		log.Printf("Apply: %v\n", app.Id)
+		log.Printf("eventlogger: Apply: %v\n", app.Id)
 	}
 }
 
 func (logger *EventLoggerModule) AddTask(task *core.AppBackend, app *core.AppCluster) {
-	log.Printf("Task Add: %v: %v %v\n", task.State, app.Id, task.Host)
+	log.Printf("eventlogger: Task Add: %v: %v %v\n", task.State, app.Id, task.Host)
 }
 
 func (logger *EventLoggerModule) RemoveTask(task *core.AppBackend, app *core.AppCluster) {
-	log.Printf("Task Remove: %v: %v %v\n", task.State, app.Id, task.Host)
+	log.Printf("eventlogger: Task Remove: %v: %v %v\n", task.State, app.Id, task.Host)
 }
