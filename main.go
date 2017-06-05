c8568dc050bd46c3194439123d7af29f347d7459
Martin Bormeister <mbormeister@dawandamail.com>
2017-06-05 15:59:33 +0200

    release 0.9.21

--- main.go
+++ main.go
@@ -458,7 +458,7 @@ func (mmsd *mmsdService) MaybeResetFromTasks(force bool) error {
 	return nil
 }
 
-const appVersion = "0.9.20"
+const appVersion = "0.9.21"
 const appLicense = "MIT"
 
 func showVersion() {
